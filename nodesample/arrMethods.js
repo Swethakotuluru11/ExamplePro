6eb2aaa5e5145cfce04b86ebb3afc3fccab9f0a4
Swetha <kotuluru.swetha@capgemini.com>
2024-11-25 13:24:30 +0530

    sample

---
+++ nodesample/arrMethods.js
@@ -0,0 +1,17 @@
+//Array Length Method
+let arr=['ram','swetha','shiva'];
+console.log(arr.length);
+arr.push('prasad');
+console.log(arr.length);
+arr.pop[2];
+console.log(arr);
+arr.length=7;//taking more legth
+console.log(arr);
+arr.length=2;//fixed size length
+console.log(arr);
+arr.length=0;
+console.log(arr);
+let abc=[];
+console.log(abc);
+console.log(abc.length);
+
